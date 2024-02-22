39f44434f2a979ad5f261fa5366143ed61073d27
Sayandip Adhikary <sayandipadhikary@Sayandips-MacBook-Air.local>
2024-02-22 20:25:50 +0530

    json added

--- index.js
+++ index.js
@@ -3,12 +3,41 @@ require('dotenv').config()
 const app = express()
 const port = 3000
 
+const jsonData = {
+    "person": {
+        "name": "John Doe",
+        "age": 30,
+        "address": {
+            "city": "Randomville",
+            "country": "Wonderland"
+        },
+        "contacts": [
+            {
+                "type": "email",
+                "value": "john.doe@example.com"
+            },
+            {
+                "type": "phone",
+                "value": "+1234567890"
+            }
+        ],
+        "isStudent": false
+    },
+    "randomArray": [1, "apple", true, null, { "key": "value" }],
+    "timestamp": 1645632000000
+}
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.send('Hello, <h1>Please Login</h1>')
+})
+
+app.get('/json', (req, res) => {
+    res.json(jsonData)
 })
 
 app.listen(process.env.PORT, () => {
